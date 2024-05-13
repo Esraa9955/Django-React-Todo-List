e8a298287ac391db770c9c8ea2c647f4c05a3560
Esraa Abd El_Maksoud <abdelmaksoudesraa99@gmail.com>
2024-05-13 04:44:51 +0300

    login&logout navebar

--- frontend/src/views/Navbar.js
+++ frontend/src/views/Navbar.js
@@ -1,53 +1,80 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import AuthContext from '../context/AuthContext'
+import { jwtDecode } from "jwt-decode";
+
 function Navbar() {
+  const { user, logoutUser } = useContext(AuthContext);
+  let user_id = null;
+  const token = localStorage.getItem("authToken");
+
+  if (token) {
+    const decoded = jwtDecode(token);
+    user_id = decoded.user_id;
+  }
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
-    <div className="container-fluid">
-      <Link className="navbar-brand" to="h">
-        <img
-          style={{ width: 100, height: 40, objectFit: "contain" }}
-          src="https://i.imgur.com/juL1aAc.png"
-          alt=""
-        />
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="h">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="h">
-              Features
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="h">
-              Pricing
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link disabled">Disabled</Link>
-          </li>
-        </ul>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="h">
+          <img
+            style={{ width: 100, height: 40, objectFit: "contain" }}
+            src="https://i.imgur.com/juL1aAc.png"
+            alt=""
+          />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            {!user && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="login">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="register">
+                    Register
+                  </Link>
+                </li>
+              </>
+            )}
+            {user && (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="h">
+                    Dashboard
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" onClick={logoutUser}>
+                    Logout
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-  
-  )
+    </nav>
+  );
 }
 
 export default Navbar
